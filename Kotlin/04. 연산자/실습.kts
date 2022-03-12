2e9b1a2b346320f351e23f9ed34a06365cd8421c
gayeon00 <chlrkdus4819@gmail.com>
2022-03-12 18:16:22 +0900

    feat: 연산자, 메서드(이론만) 학습

---
+++ Kotlin/04. 연산자/실습.kts
@@ -0,0 +1,63 @@
+//산술연산자
+val number1 = 1 + 10
+val number2 = 20 - 4
+val number3 = 40 * 5
+val number4 = 40 / 5
+val number5 = 40 % 5
+
+//대입연산자
+var number6 = 50
+number6 = 3939
+println(number6)
+
+//복합대입연산자
+val number8: Int = 12
+number8 += 8
+number8 -= 8
+number8 *= 8
+number8 /= 8
+number8 %= 8
+
+//증감연산자
+val number9: Int = 10
+val number10: Int = 10
+println(number9++)
+println(++number10)
+
+//비교연산자
+val number12: Int = 1
+val number13: Int = 2
+val result: Boolean = number12 > number13
+println(result)
+val result2: Boolean = number12 == number13
+println(result2)
+val result3: Boolean = number12 != number13
+println(result3)
+
+//논리연산자
+val boolean1: Boolean = true
+val boolean2: Boolean = false
+val result4: Boolean = boolean1 && boolean2
+println(result4)
+val result5: Boolean = boolean1 || boolean2
+println(result5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
